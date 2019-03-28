adc03ce80c79b4d26340f000f97ad2ed57cdc255
wallaceiam <chris@mymojo.ca>
2019-03-28 20:06:36 +0000

    Upgrading React

--- ios/wealthone/AppDelegate.m
+++ ios/wealthone/AppDelegate.m
@@ -16,11 +16,7 @@
 {
   NSURL *jsCodeLocation;
 
-#ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"wealthone"
